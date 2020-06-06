f9e98e674d34f46877854baada7761fd58715620
Saint Cyr <saintcyrwin@gmail.com>
2020-06-06 11:34:47 +0000

    Initialisation

---
+++ components/DeliveryAdress.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeliveryAdress = () => {
+  return <div></div>;
+};
+
+export default DeliveryAdress;
---
+++ components/Home.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const Home = () => {
+  return <View></View>;
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#39F",
+  },
+});
